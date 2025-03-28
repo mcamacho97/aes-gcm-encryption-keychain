87484f453b0a5eedf83e87f91694bad3ab3ac436
Mauricio Camacho <mcamacho@SISTEMATICA162.local>
2025-03-28 15:38:43 -0600

    removed keychain file

--- utils/keychain.ts
+++
@@ -1,6 +0,0 @@
-import * as Keychain from "react-native-keychain";
-
-export const saveKeysToKeychain = async (key:any, iv:any)=> {
-    await Keychain.setGenericPassword("aesKey", key);
-    await Keychain.setGenericPassword("iv", iv);
-} 
